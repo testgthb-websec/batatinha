744f820f56dd8d6e90bdd15afc8a3f33e120e384
GLMIRA <gllimamira@gmail.com>
2025-10-20 21:25:05 -0300

    primeiro commit

---
+++ test.py
@@ -0,0 +1,19 @@
+option = 0
+while True:
+    print("Menu:")
+    print("1. soma 1")
+    print("2. subtração 2")
+    print("3. Exit")
+    try:
+        option = int(input("Select an option (1-3): "))
+        if option == 1:
+            print("You selected Option 1.")
+        elif option == 2:
+            print("You selected Option 2.")
+        elif option == 3:
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid option. Please select a number between 1 and 3.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
